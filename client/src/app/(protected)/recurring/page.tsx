fdf362b4883d7db5c6b6026504217908d7f181a8
Alexis De Leon Rodriguez <alexisdlr3009@hotmail.com>
2026-06-12 14:55:30 -0500

    client: todo: recurring bills

--- client/src/app/(protected)/recurring/page.tsx
+++ client/src/app/(protected)/recurring/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import RecurringContent from "@/components/recurring/recurring-content";
-import { useRecurringBills } from "@/hooks/recurring/use-recurring-bills";
+// import { useRecurringBills } from "@/hooks/recurring/use-recurring-bills";
 
 const RecurringPage = () => {
-  const recurringQuery = useRecurringBills();
-  console.log("recurring info", recurringQuery);
+  // const recurringQuery = useRecurringBills();
   return (
-    <RecurringContent
-      dueSoonTotal={recurringQuery.dueSoonTotal}
-      dueSoonCount={recurringQuery.dueSoonCount}
-      recurringBills={recurringQuery.recurringBills}
-      totalBills={recurringQuery.totalBills}
-    />
+    // <RecurringContent
+    //   dueSoonTotal={recurringQuery.dueSoonTotal}
+    //   dueSoonCount={recurringQuery.dueSoonCount}
+    //   recurringBills={recurringQuery.recurringBills}
+    //   totalBills={recurringQuery.totalBills}
+    // />
+    <div>Hello</div>
   );
 };
 
