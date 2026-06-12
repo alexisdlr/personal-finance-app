0f4df83d7ff44a0eb2616e5324d2d7169cf85eca
Alexis De Leon Rodriguez <alexisdlr3009@hotmail.com>
2026-06-12 14:54:49 -0500

    client: todo: recurring bills

--- client/src/hooks/recurring/use-recurring-bills.ts
+++ client/src/hooks/recurring/use-recurring-bills.ts
@@ -1,29 +1,29 @@
-import useFetchOverviewData from "../overview/use-get-overview-data";
+// import useFetchOverviewData from "../overview/use-get-overview-data";
 
-export const useRecurringBills = () => {
-  const overviewQuery = useFetchOverviewData();
-  const recurringBills = overviewQuery.data?.data.recurringBills ?? [];
-  const totalBills = overviewQuery.data?.data.totalBills ?? 0;
+// export const useRecurringBills = () => {
+//   const overviewQuery = useFetchOverviewData();
+//   const recurringBills = overviewQuery.data?.data.recurringBills ?? [];
+//   const totalBills = overviewQuery.data?.data.totalBills ?? 0;
 
-  const dueSoon = recurringBills.filter((bill) => {
-    const day = new Date(bill.date).getDate();
+//   const dueSoon = recurringBills.filter((bill) => {
+//     const day = new Date(bill.date).getDate();
 
-    return day <= 7;
-  });
+//     return day <= 7;
+//   });
 
-  const dueSoonTotal = dueSoon.reduce(
-    (acc, bill) => acc + Math.abs(bill.amount),
-    0,
-  );
+//   const dueSoonTotal = dueSoon.reduce(
+//     (acc, bill) => acc + Math.abs(bill.amount),
+//     0,
+//   );
 
-  return {
-    recurringBills,
-    totalBills,
-    dueSoon,
-    dueSoonTotal,
-    dueSoonCount: dueSoon.length,
+//   return {
+//     recurringBills,
+//     totalBills,
+//     dueSoon,
+//     dueSoonTotal,
+//     dueSoonCount: dueSoon.length,
 
-    isLoading: overviewQuery.isLoading,
-    error: overviewQuery.error,
-  };
-};
+//     isLoading: overviewQuery.isLoading,
+//     error: overviewQuery.error,
+//   };
+// };
